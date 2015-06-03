3c9fd9e52892108739004b9961682a445649cc24
Hu Keping <hukeping@huawei.com>
2015-06-03 19:29:42 +0800

    Example: Add example for string
    Signed-off-by: Hu Keping <hukeping@huawei.com>

---
+++ example/example_string.go
@@ -0,0 +1,28 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/HuKeping/rbtree"
+)
+
+func main() {
+	rbt := rbtree.New()
+
+	rbt.Insert(rbtree.String("Hello"))
+	rbt.Insert(rbtree.String("World"))
+
+	rbt.Ascend(rbt.Min(), Print)
+}
+
+func Print(item rbtree.Item) bool {
+	i, ok := item.(rbtree.String)
+	if !ok {
+		return false
+	}
+	fmt.Println(i)
+	return true
+}
